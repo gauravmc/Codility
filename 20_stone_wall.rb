d7a07bf1f92c145ac91f2b6f2067bc8f11cf3cb1
Gaurav Chande <gmail@gauravchande.com>
2018-12-11 14:37:13 +0530

    StoneWall

---
+++ 20_stone_wall.rb
@@ -0,0 +1,41 @@
+# Write a function:
+
+#     def solution(h)
+
+# that, given an array H of N positive integers specifying the height of the wall, returns the minimum number of blocks needed to build it.
+
+BLOCK_STACK = []
+
+def solution(h)
+  blocks = 0
+  blocks = add_new_block(blocks, h.first)
+
+  (1...h.size).each do |i|
+    last_ht, ht = BLOCK_STACK.pop, h[i]
+    if last_ht > ht
+      while last_ht && last_ht > ht
+        last_ht = BLOCK_STACK.pop
+      end
+
+      if last_ht == ht
+        BLOCK_STACK.push(last_ht)
+      else
+        BLOCK_STACK.push(last_ht)
+        blocks = add_new_block(blocks, ht)
+      end
+    elsif last_ht < ht
+      BLOCK_STACK.push(last_ht)
+      blocks = add_new_block(blocks, ht)
+    elsif last_ht == ht
+      BLOCK_STACK.push(last_ht)
+    end
+  end
+
+  blocks
+end
+
+def add_new_block(total, value)
+  total += 1
+  BLOCK_STACK.push(value)
+  total
+end
